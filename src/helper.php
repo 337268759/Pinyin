8a9676b8ed5ad02b61641b2e0a06c95c76d283c5
Jun <192.168.0.130@mobisist.com>
2017-04-27 14:09:23 +0800

    '.'

--- src/helper.php
+++ src/helper.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * 将字符串转为拼音
+ *
+ * @param string $String 中文字符串
+ * @param string $linkString 链接字符串
+ * @param string $type 翻译类型
+ *
+ * @return mixed
+ */
 function Pinyin( $String = '', $linkString = '', $type = 'permalink' ) {
 
 	$pinyin = new Jun\Pinyin\Pinyin();
